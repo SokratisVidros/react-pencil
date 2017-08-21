3f0c9453e819a21907c3cfe025f0fd69a0880718
Sokratis Vidros <sokratis.vidros@gmail.com>
2017-08-21 13:04:23 +0300

    Build latest version.

--- dist/react-pencil.js
+++ dist/react-pencil.js
@@ -8,6 +8,10 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -28,15 +32,14 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Component = _react2.default.Component,
-    PropTypes = _react2.default.PropTypes;
+var Component = _react2.default.Component;
 
 
 var commonPropTypes = {
-  finishEdit: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  tabIndex: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  value: PropTypes.string
+  finishEdit: _propTypes2.default.func.isRequired,
+  placeholder: _propTypes2.default.string,
+  tabIndex: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.number]),
+  value: _propTypes2.default.string
 };
 
 var commonDefaultProps = {};
@@ -338,7 +341,7 @@ var ReactPencil = function (_Component3) {
         { className: 'react-pencil' + (wrapperClassname ? ' ' + wrapperClassname : '') + (error ? ' error' : '') },
         _react2.default.createElement(
           'div',
-          { className: 'input' },
+          { className: 'input-field' },
           _react2.default.createElement(Component, _extends({ ref: 'editable' }, rest, { finishEdit: this.finishEdit.bind(this) })),
           pencil ? this.renderPencilButton() : null
         ),
@@ -352,11 +355,11 @@ var ReactPencil = function (_Component3) {
 
 Object.assign(ReactPencil, {
   propTypes: {
-    error: PropTypes.string,
-    multiline: PropTypes.bool,
-    name: PropTypes.string,
-    onEditDone: PropTypes.func,
-    value: PropTypes.string
+    error: _propTypes2.default.string,
+    multiline: _propTypes2.default.bool,
+    name: _propTypes2.default.string,
+    onEditDone: _propTypes2.default.func,
+    value: _propTypes2.default.string
   },
   defaultProps: {
     value: '',
